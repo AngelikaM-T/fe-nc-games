6c814c7ef8766cc7b58135e1b921048386257880
Angelika Tiangson <angelika@Angelikas-MacBook-Pro.local>
2022-12-06 13:40:46 +0000

    view individual review

--- src/util/api.js
+++ src/util/api.js
@@ -1,14 +1,17 @@
 import axios from "axios";
 
 const gamesApi = axios.create({
-  baseURL: 'https://magnificent-hare-sheath-dress.cyclic.app/api',
+  baseURL: "https://magnificent-hare-sheath-dress.cyclic.app/api",
 });
 
 export const getReviews = () => {
-  return gamesApi
-  .get('/reviews').then(({data}) => {
-    return data
+  return gamesApi.get("/reviews").then(({ data }) => {
+    return data;
   });
 };
 
-
+export const getReviewById = (review_id) => {
+  return gamesApi.get(`/reviews/${review_id}`).then(({ data }) => {
+    return data.review;
+  });
+};
